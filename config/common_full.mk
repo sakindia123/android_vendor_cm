0a7b2e8355f45afa37bae48ae8dfc5629f668fb4
beerbong <yghazi@gmail.com>
2012-12-04 18:19:18 +0100

    Restoring wallpapers 3
    Change-Id: I4a3542bef4843a321af811c9348c901a4b61aa21

--- config/common_full.mk
+++ config/common_full.mk
@@ -14,13 +14,12 @@ $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 include vendor/cm/config/cm_audio.mk
 
 # Optional CM packages
-#beerbong
-#PRODUCT_PACKAGES += \
-#    HoloSpiralWallpaper \
-#    MagicSmokeWallpapers \
-#    NoiseField \
-#    Galaxy4 \
-#    LiveWallpapers \
-#    LiveWallpapersPicker \
-#    VisualizationWallpapers \
-#    PhaseBeam
+PRODUCT_PACKAGES += \
+    HoloSpiralWallpaper \
+    MagicSmokeWallpapers \
+    NoiseField \
+    Galaxy4 \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    PhaseBeam
